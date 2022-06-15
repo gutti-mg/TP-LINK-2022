39b7745359e23f13671ee7f56244c06ad7542c0d
Mariano Antonio Gutierrez Filgueira <MG_E91219@redlink.com.ar>
2022-06-15 10:25:28 -0300

    Recuperacion del tp, un poco mas ordenado

---
+++ main/java/ar/edu/utn/tpIntegrador/Cliente.java
@@ -0,0 +1,95 @@
+package ar.edu.utn.tpIntegrador;
+
+import java.util.List;
+
+public class Cliente {
+
+	private String nombre;
+	private Number fechaNacimiento;
+	private Enum<tipoDocumento> tipoDocumento;
+	private String nroDocumento;
+	private String telefono;
+	private String email;
+	private List<OrdenCompra> comprasRealizadas;
+	//private Enum<medioDePago> medioDePago;
+	private String medioDePago;
+	private Boolean esSocio;
+	private CarritoDeCompras carritoCliente;
+
+	
+	public Cliente(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Number getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Number fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public Enum<tipoDocumento> getTipoDocumento() {
+		return tipoDocumento;
+	}
+	public void setTipoDocumento(Enum<tipoDocumento> tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+	public String getNroDocumento() {
+		return nroDocumento;
+	}
+	public void setNroDocumento(String nroDocumento) {
+		this.nroDocumento = nroDocumento;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public List<OrdenCompra> getComprasRealizadas() {
+		return comprasRealizadas;
+	}
+	public void setComprasRealizadas(List<OrdenCompra> comprasRealizadas) {
+		this.comprasRealizadas = comprasRealizadas;
+	}
+	
+	
+	/*public Enum<medioDePago> getMedioDePago() {
+		return medioDePago;
+	}
+	public void setMedioDePago(Enum<medioDePago> medioDePago) {
+		this.medioDePago = medioDePago;
+	}*/
+	
+	public Boolean esSocio() {
+		return esSocio;
+	}
+	public void setSocio(Boolean socio) {
+		this.esSocio = socio;
+	}
+	public String getMedioDePago() {
+		return medioDePago;
+	}
+	public void setMedioDePago(String medioDePago) {
+		this.medioDePago = medioDePago;
+	}
+
+	public CarritoDeCompras getCarritoCliente() {
+		return carritoCliente;
+	}
+
+	public void setCarritoCliente(CarritoDeCompras carritoCliente) {
+		this.carritoCliente = carritoCliente;
+	}
+
+}
